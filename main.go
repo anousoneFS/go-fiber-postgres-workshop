d0c07b89e909f70edc2a48c8b6edab10f04fb26e
anousone <anousone.litd@gmail.com>
2022-06-18 14:51:12 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "postgres://oiaaglbm:M7yp7cg1uAG4UpiVazViExpoYwnZTdIw@tiny.db.elephantsql.com/oiaaglbm"
+	dial := postgres.Open(dsn)
+	db, err := gorm.Open(dial)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.AutoMigrate(User{}); err != nil {
+		panic(err)
+	}
+
+	// db.Migrator().CreateTable(User{})
+}
+
+// table: User
+type User struct {
+	Name string
+	Age  int
+}
