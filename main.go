a097b3df07c4b1ad756c43cb0d251228b9e5879e
anousone <anousone.litd@gmail.com>
2022-06-18 15:53:57 +0700

    add email

--- main.go
+++ main.go
@@ -22,15 +22,18 @@ func main() {
 	}
 	// db.Migrator().CreateTable(User{})
 
-	CreateUser("anousone", "myemail@gmail.com", 23)
+	// CreateUser("daky", "daky@gmail.com", 23)
+	// GetAllUser()
+	// GetUserByID(8)
+	GetUserByName("anousone")
 }
 
 // table: User
 type User struct {
-	ID    int    // default ID is primarykey
 	Name  string `gorm:"column:myname;size:8;not null"`
 	Email string `gorm:"not null"`
 	Age   int    `gorm:"not null"`
+	gorm.Model
 }
 
 func (u User) TableName() string {
@@ -43,5 +46,37 @@ func CreateUser(name, email string, age int) {
 	if err := tx.Error; err != nil {
 		fmt.Printf("CreateUser():%v\n", err)
 	}
-	fmt.Printf("result: %+v\n", u)
+	fmt.Printf("result: %+v\n", u.ID)
+}
+
+func GetAllUser() {
+	user := []User{}
+	if err := db.Find(&user).Error; err != nil {
+		fmt.Printf("GetAllUser():%v\n", err)
+		return
+	}
+	for index, item := range user {
+		fmt.Printf("%v name:%v, email:%v, age:%v\n", index, item.Name, item.Email, item.Age)
+	}
+	// fmt.Printf("user:%+v\n", user)
+}
+
+func GetUserByID(id int) {
+	user := User{}
+	tx := db.Where("id=?", id).Find(&user)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	fmt.Printf("name:%v, email:%v, age:%v\n", user.Name, user.Email, user.Age)
+}
+
+func GetUserByName(name string) {
+	user := User{}
+	tx := db.Where("myname=?", name).Find(&user)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	fmt.Printf("name:%v, email:%v, age:%v\n", user.Name, user.Email, user.Age)
 }
